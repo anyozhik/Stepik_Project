58789082d0ef5571b1d9fe54851217ee41ae0468
unknown <ateslyuk5@gmail.com>
2024-12-12 23:53:40 +0700

    Adding a file with locators, changing page's file using locators

---
+++ pages/locators.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+#Каждый класс будет соответствовать каждому классу PageObject: 
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+
+    
