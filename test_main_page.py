821a4ae8a532244c81cc6a1d6e7a0ff139cd2c3e
unknown <ateslyuk5@gmail.com>
2024-12-12 23:26:12 +0700

    Dealing with NoElementException

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,15 @@
 from .pages.main_page import MainPage
 
+link="http://selenium1py.pythonanywhere.com/"
+
+def test_should_be_login_link(browser):
+    page=MainPage(browser, link) #инициализируем PageObject, передаем в конструктор экземпляр драйвера и url
+    page.open()
+    page.should_be_login_link()
+
 def test_guest_can_go_to_login_page(browser):
-    link="http://selenium1py.pythonanywhere.com/"
     page=MainPage(browser, link) #инициализируем PageObject, передаем в конструктор экземпляр драйвера и url
     page.open()
     page.go_to_login_page()
     
+
