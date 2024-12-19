72b66cadcc2f50c72c07992ef18524375da880fc
unknown <ateslyuk5@gmail.com>
2024-12-19 14:36:28 +0700

    Moving two methods from main page to base page and updating test

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,6 +4,8 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import math
+import time
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -12,8 +14,18 @@ class BasePage():
         self.url=url
         self.browser.implicitly_wait(timeout)
 
-    def open(self):
-        self.browser.get(self.url)
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+# is_disappeared: будет ждать до тех пор, пока элемент не исчезнет
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+            until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
 
     def is_element_present(self, how, what):
         try:
@@ -30,16 +42,9 @@ class BasePage():
             return True
         return False
 
-# is_disappeared: будет ждать до тех пор, пока элемент не исчезнет
-    def is_disappeared(self, how, what, timeout=4):
-        try:
-            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
-            until_not(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return False
-        return True
+    def open(self):
+        self.browser.get(self.url)
 
-    
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
@@ -54,6 +59,15 @@ class BasePage():
         except NoAlertPresentException:
             print("No second alert presented")
  
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+
+
+
+    
+
     
 
 
--- test_product_page.py
+++ test_product_page.py
@@ -29,10 +29,23 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_success_message()
 
-
 @pytest.mark.xfail  
 def test_message_diappeared_after_adding_product_to_basket(browser):
     page=ProductPage(browser, link) 
     page.open()
     page.add_to_basket()
     page.should_success_message_disappear()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page=ProductPage(browser, link) 
+    page.open()
+    page.go_to_login_page()
+
+
