eac1c9203ae11bb53f7b6210978858672c028c55
anyozhik <85816469+anyozhik@users.noreply.github.com>
2025-05-01 15:09:59 +0700

    Update test_main_page.py
    deleting some comments

--- test_main_page.py
+++ test_main_page.py
@@ -9,12 +9,12 @@ link="http://selenium1py.pythonanywhere.com/"
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
     def test_should_be_login_link(self, browser):
-        page=MainPage(browser, link) #инициализируем PageObject, передаем в конструктор экземпляр драйвера и url
+        page=MainPage(browser, link)
         page.open()
         page.should_be_login_link()
 
     def test_guest_can_go_to_login_page(self, browser):
-        page=MainPage(browser, link) #инициализируем PageObject, передаем в конструктор экземпляр драйвера и url
+        page=MainPage(browser, link) 
         page.open()
         page.go_to_login_page()
         login_page=LoginPage(browser, browser.current_url)
